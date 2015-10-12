53b0134dcf6bca19223795da846739012db3920d
Dan Nixon <dan@dan-nixon.com>
2015-10-12 23:50:24 +0100

    Test updates
    [ci skip]

--- project1/test/Test.cpp
+++ project1/test/Test.cpp
@@ -1,21 +1,52 @@
 #include <cstdlib>
+#include <iostream>
 
 #include "Vector3DStack.h"
 #include "Quaternion.h"
 
 #define TH 0.0001
 
-int result = 0;
+size_t g_testsCount = 0;
+size_t g_testsPassed = 0;
 
-#define TS_ASSERT_EQUALS(a, b) { if (!(a == b)) result = 1; }
-#define TS_ASSERT_DIFFERS(a, b) { if (a == b) result = 1; }
-#define TS_ASSERT_DELTA(a, b, th) { if (abs(a - b) > th) result = 1; }
+// Macro for testing that two values are equal to each other.
+#define TS_ASSERT_EQUALS(a, b)                                                 \
+  {                                                                            \
+    g_testsCount++;                                                            \
+    if (a == b)                                                                \
+      g_testsPassed++;                                                         \
+  }
 
-//TODO: Test functions
+// Macro for testing that two values are not equal to each other.
+#define TS_ASSERT_DIFFERS(a, b)                                                \
+  {                                                                            \
+    g_testsCount++;                                                            \
+    if (!(a == b))                                                             \
+      g_testsPassed++;                                                         \
+  }
+
+// Macro for testing two numerical values are close to each other within a
+// tolerance.
+#define TS_ASSERT_DELTA(a, b, th)                                              \
+  {                                                                            \
+    g_testsCount++;                                                            \
+    if (abs(a - b) <= th)                                                      \
+      g_testsPassed++;                                                         \
+  }
+
+// TODO: Test functions
 
 int main()
 {
-  //TODO: Test function calls
+  // TODO: Test function calls
+  TS_ASSERT_EQUALS(true, true);
+  TS_ASSERT_EQUALS(true, true);
+  TS_ASSERT_EQUALS(true, false);
 
-  return result;
+  // Output the test results
+  std::cout << "Tests passed: " << g_testsPassed << "/" << g_testsCount << " ("
+            << ((100 * g_testsPassed) / g_testsCount) << "%)" << std::endl;
+
+  // Return 0 for no failed tests, or 1 if at least one test failed
+  return (g_testsPassed != g_testsCount);
 }
